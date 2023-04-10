a2658daafff34d81f30478ccca41d95351e52394
SpaceOddity7 <pino.gomez.silvi@gmail.com>
2023-04-10 13:06:35 +0200

    update2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Documents from './components/common/Documents';
 import Holidays from './components/common/Holidays';
 import Personal from './components/common/Personal';
+import NavbarMaster from './components/header/NavbarMaster';
 import Signin from './components/common/Signin';
 import Workshops from './components/common/Workshops';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Documents />
+      <NavbarMaster />
       <Holidays />
       <Personal />
       <Signin />
